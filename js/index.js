f0ca54b6c095d52117c32e0c0f0bd9aeed532e2c
Facundo Wertmuller <fwertmuller@gmail.com>
2021-09-04 14:56:16 -0300

    first_commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let headphones = document.getElementById('black_headphone')
+
+
+function black(){
+    headphones.src = "media/black_headphones_web.png"
+}
+function orange(){
+    headphones.src = "media/orange_headphones_web.png"
+}
+
+function golden(){
+    headphones.src = "media/golden_headphones_web.png"
+}
